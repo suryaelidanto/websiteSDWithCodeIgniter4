939e41038b9b3e32350a3ddbf2a7ffba7c3d1ffb
Cintara Surya Elidanto <suryaelidanto@gmail.com>
2020-11-15 14:51:43 +0700

    Menambahkan tampilan galeri foto, galeri video

--- websitesd/app/Views/home.php
+++ websitesd/app/Views/home.php
@@ -1,87 +1,89 @@
 <!-- CAROUSEL -->
 
 <center style="background-color: #000;">
-<div id="tes-carousel" class="carousel slide" data-ride="carousel" style="width:1024px;">
-  <!-- indikator -->
-  <ol class="carousel-indicators">
-    <li data-target="#tes-carousel" data-slide-to="0" class="active"></li>
-    <li data-target="#tes-carousel" data-slide-to="1"></li>
-    <li data-target="#tes-carousel" data-slide-to="2"></li>
-  </ol>
-    
-  <div class="carousel-inner">
-  
-    <!-- slide 1 -->
-    <div class="item active">
-      <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="Demo 1" style="max-width: 1020px; max-height: 640px;"/>
-      <!-- caption -->
-      <div class="carousel-caption">
-        <h3>Judul Caption 1</h3>
-        <p>Keterangan selanjutnya Caption 1</p>
+  <div id="tes-carousel" class="carousel slide" data-ride="carousel" style="width:1024px;">
+    <!-- indikator -->
+    <ol class="carousel-indicators">
+      <li data-target="#tes-carousel" data-slide-to="0" class="active"></li>
+      <li data-target="#tes-carousel" data-slide-to="1"></li>
+      <li data-target="#tes-carousel" data-slide-to="2"></li>
+    </ol>
+
+    <div class="carousel-inner">
+
+      <!-- slide 1 -->
+      <div class="item active">
+        <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="Demo 1" style="max-width: 1020px; max-height: 640px;" />
+        <!-- caption -->
+        <div class="carousel-caption">
+          <h3>Judul Caption 1</h3>
+          <p>Keterangan selanjutnya Caption 1</p>
+        </div>
       </div>
-    </div>
-    
-    <!-- slide 2 -->
-    <div class="item">
-      <img src="https://miro.medium.com/max/10790/1*svDSkSCaSXqBXPnM3cy63A.png" alt="Demo 2"   style="max-width: 1020px; max-height: 640px;"/>
-      <!-- caption -->
-      <div class="carousel-caption">
-        <h3>Judul Caption 2</h3>
-        <p>Keterangan selanjutnya Caption 2</p>
+
+      <!-- slide 2 -->
+      <div class="item">
+        <img src="https://miro.medium.com/max/10790/1*svDSkSCaSXqBXPnM3cy63A.png" alt="Demo 2" style="max-width: 1020px; max-height: 640px;" />
+        <!-- caption -->
+        <div class="carousel-caption">
+          <h3>Judul Caption 2</h3>
+          <p>Keterangan selanjutnya Caption 2</p>
+        </div>
       </div>
-    </div>
-    
-    <!-- slide 3 -->
-    <div class="item">
-      <img src="https://i.pinimg.com/originals/e4/81/cb/e481cbc93bb6e063fe3029186383c855.jpg" alt="Demo 3"   style="max-width: 1020px; max-height: 640px;"/>
-      <!-- caption -->
-      <div class="carousel-caption">
-        <h3>Judul Caption 3</h3>
-        <p>Keterangan selanjutnya Caption 3</p>
+
+      <!-- slide 3 -->
+      <div class="item">
+        <img src="https://i.pinimg.com/originals/e4/81/cb/e481cbc93bb6e063fe3029186383c855.jpg" alt="Demo 3" style="max-width: 1020px; max-height: 640px;" />
+        <!-- caption -->
+        <div class="carousel-caption">
+          <h3>Judul Caption 3</h3>
+          <p>Keterangan selanjutnya Caption 3</p>
+        </div>
       </div>
+
     </div>
-    
+
+    <!-- kontrol-->
+    <a class="carousel-control left" href="#tes-carousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control right" href="#tes-carousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
   </div>
-  
-  <!-- kontrol-->
-  <a class="carousel-control left" href="#tes-carousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control right" href="#tes-carousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  
-</div>
 </center>
 
-
-<!-- BERITA TERBARU -->
-<div style="background-color: #fff; padding: 50px;">
-
-</div>
-
-<!-- Perjudulan -->
-<div style="background-color: #FFD500; width:300px; height: 50px; color: white; margin-left: 50px;">
-<center>
-  <h1 style="line-height: 50px;"> Berita Terbaru </h1>
-</center>
-</div>
-<!-- Akhir -->
-
-<div style="background-color: #FFD500; height: 500px;">
-
-<div style="padding:80px;" class="row">
-<!-- artikel -->
-<div class="col-lg-6 row" style="margin-top: 30px;">  
-          
-            <div class="col-lg-2 text-center" style="background-color: #fff; width: 50px; height: 50px; margin-right:20px;">
-              <b> 15 NOV </b> 
-            </div>
+<body>
 
 
-<div class="col-lg-10 row">
+  <!-- BERITA TERBARU -->
+  <div style="background-color: #fff; padding: 50px;">
+
+  </div>
+
+  <!-- Perjudulan -->
+  <div style="background-color: #FFD500; width:300px; height: 50px; color: white; margin-left: 50px;">
+    <center>
+      <h1 style="line-height: 50px;"> Berita Terbaru </h1>
+    </center>
+  </div>
+  <!-- Akhir -->
+
+  <div style="background-color: #FFD500; height: 500px;">
+
+    <div style="padding:80px;">
+      <!-- artikel -->
+      <div class="col-lg-6 row" style="margin-top: 30px;">
+
+        <div class="col-lg-2 text-center" style="background-color: #fff; width: 50px; height: 50px; margin-right:20px;">
+          <b> 15 NOV </b>
+        </div>
+
+
+        <div class="col-lg-10 row">
 
           <div class="col-lg-12 panel panel-default">
             <div class="panel-body">
@@ -89,34 +91,204 @@
             </div>
           </div>
 
-            <div class="col-lg-12 row text-center">
-                  <div class="col-lg-6" style="background-color: #fff; width: 100px; height: 25px;float:left; display: inline-block;">
-                    <p style="line-height: 25px;"> Berita</p>
-                  </div>
-
-                  <div class="col-lg-6" style="background-color: #fff; width: 120px; height: 25px;float:right; display: inline-block;">
-                    <a style="line-height: 25px;" href="#"> Selengkapnya... </a>
-                  </div>
-              
+          <div class="col-lg-12 row text-center">
+            <div class="col-lg-6" style="background-color: #fff; width: 100px; height: 25px;float:left; display: inline-block;">
+              <p style="line-height: 25px;"> Berita</p>
             </div>
-          
 
+            <div class="col-lg-6" style="background-color: #fff; width: 120px; height: 25px;float:right; display: inline-block;">
+              <a style="line-height: 25px;" href="#"> Selengkapnya... </a>
+            </div>
 
-</div>
-
-
-
-
-</div>
+          </div>
 
 
 
-<!-- akhir artikel -->
-
-</div>
-
-<!-- Akhir berita terbaru -->
+        </div>
 
 
 
-<body>
+
+      </div>
+
+
+
+
+
+
+      <div class="col-lg-6 row" style="margin-top: 30px;">
+
+        <div class="col-lg-2 text-center" style="background-color: #fff; width: 50px; height: 50px; margin-right:20px;">
+          <b> 15 NOV </b>
+        </div>
+
+
+        <div class="col-lg-10 row">
+
+          <div class="col-lg-12 panel panel-default">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto omnis quisquam corporis accusantium nam ratione inventore voluptas, voluptatum quasi non, doloribus doloremque? Debitis deleniti sint corrupti laudantium. Provident, architecto accusamus!
+            </div>
+          </div>
+
+          <div class="col-lg-12 row text-center">
+            <div class="col-lg-6" style="background-color: #fff; width: 100px; height: 25px;float:left; display: inline-block;">
+              <p style="line-height: 25px;"> Berita</p>
+            </div>
+
+            <div class="col-lg-6" style="background-color: #fff; width: 120px; height: 25px;float:right; display: inline-block;">
+              <a style="line-height: 25px;" href="#"> Selengkapnya... </a>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+
+
+      <div class="col-lg-6 row" style="margin-top: 30px;">
+
+        <div class="col-lg-2 text-center" style="background-color: #fff; width: 50px; height: 50px; margin-right:20px;">
+          <b> 15 NOV </b>
+        </div>
+
+
+        <div class="col-lg-10 row">
+
+          <div class="col-lg-12 panel panel-default">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto omnis quisquam corporis accusantium nam ratione inventore voluptas, voluptatum quasi non, doloribus doloremque? Debitis deleniti sint corrupti laudantium. Provident, architecto accusamus!
+            </div>
+          </div>
+
+          <div class="col-lg-12 row text-center">
+            <div class="col-lg-6" style="background-color: #fff; width: 100px; height: 25px;float:left; display: inline-block;">
+              <p style="line-height: 25px;"> Berita</p>
+            </div>
+
+            <div class="col-lg-6" style="background-color: #fff; width: 120px; height: 25px;float:right; display: inline-block;">
+              <a style="line-height: 25px;" href="#"> Selengkapnya... </a>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+      </div>
+
+
+
+
+      <div class="col-lg-6 row" style="margin-top: 30px;">
+
+        <div class="col-lg-2 text-center" style="background-color: #fff; width: 50px; height: 50px; margin-right:20px;">
+          <b> 15 NOV </b>
+        </div>
+
+
+        <div class="col-lg-10 row">
+
+          <div class="col-lg-12 panel panel-default">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto omnis quisquam corporis accusantium nam ratione inventore voluptas, voluptatum quasi non, doloribus doloremque? Debitis deleniti sint corrupti laudantium. Provident, architecto accusamus!
+            </div>
+          </div>
+
+          <div class="col-lg-12 row text-center">
+            <div class="col-lg-6" style="background-color: #fff; width: 100px; height: 25px;float:left; display: inline-block;">
+              <p style="line-height: 25px;"> Berita</p>
+            </div>
+
+            <div class="col-lg-6" style="background-color: #fff; width: 120px; height: 25px;float:right; display: inline-block;">
+              <a style="line-height: 25px;" href="#"> Selengkapnya... </a>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+
+
+      </div>
+
+      <!-- akhir artikel -->
+
+    </div>
+
+    <!-- Akhir berita terbaru -->
+
+
+
+    <!-- Galeri Foto  -->
+  </div>
+
+  <div style="background-color: #fff; padding: 70px; margin-top:100px;">
+
+    <h1 class="text-center"> Galeri Foto </h1>
+    <hr>
+    <center style="margin-top: 50px;">
+
+      <div class="container row">
+        <div class="col-lg-4">
+          <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+        </div>
+
+        <div class="col-lg-4">
+          <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+        </div>
+
+        <div class="col-lg-4">
+          <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+        </div>
+
+      </div>
+
+    </center>
+
+    <!-- Akhir Galeri Foto -->
+
+    <!-- Galeri Video -->
+
+
+    <div style="background-color: #fff; padding: 70px; margin-top: 200px;">
+
+      <h1 class="text-center"> Galeri Video </h1>
+      <hr>
+      <center style="margin-top: 50px;">
+
+        <div class="container row">
+          <div class="col-lg-4">
+            <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+          </div>
+
+          <div class="col-lg-4">
+            <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+          </div>
+
+          <div class="col-lg-4">
+            <img src="https://cdn.hswstatic.com/gif/10-breathtaking-views-1-orig.jpg" alt="..." class="img-thumbnail img-responsive">
+          </div>
+
+        </div>
+
+      </center>
+
+      <!-- Akhir Galeri Video -->
