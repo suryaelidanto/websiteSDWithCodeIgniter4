baaa01b27e3db11868a642685c4a840c30371f80
Cintara Surya Elidanto <suryaelidanto@gmail.com>
2020-11-19 20:07:10 +0700

    Membuat bagian visi misi, ui , dan menganimasikan dengan AOS css

---
+++ websitesd/app/Views/profilkaryawan.php
@@ -0,0 +1,30 @@
+<!-- BAGIAN HEAD -->
+<div class="parallax" style="background-image:url('<?= base_url("assets/headerwebsd3.png") ?>'); width: auto; height: 400px;">
+    <center>
+        <div style="display: table; height: 400px; overflow: hidden;">
+            <div style="display: table-cell; vertical-align: middle;">
+                <div>
+                    <center>
+                        <h1 style="color:white;    text-shadow:
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;  "> PROFIL KARYAWAN </h1>
+                        <hr>
+                        <h3 style="color:white;    text-shadow:
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;  ">PARA KARYAWAN DAN GURU SD AL-IRSYAD 02 CILACAP</h3>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </center>
+</div>
+
+<div style="background-color:white; width: auto; padding:100px;" class="container">
+    <img src="<?= base_url("karyawan/1.jpg"); ?>" alt="Profil Karyawan" class="profil-gray">
+</div>
+
+<!-- AKHIR BAGIAN HEAD -->
--- websitesd/app/Views/visimisi.php
+++ websitesd/app/Views/visimisi.php
@@ -9,13 +9,13 @@
     -1px -1px 0 #000,
     1px -1px 0 #000,
     -1px 1px 0 #000,
-    1px 1px 0 #000;  "> BERITA TERBARU </h1>
+    1px 1px 0 #000;  "> VISI MISI </h1>
                         <hr>
                         <h3 style="color:white;    text-shadow:
     -1px -1px 0 #000,
     1px -1px 0 #000,
     -1px 1px 0 #000,
-    1px 1px 0 #000;  ">KUMPULAN BERITA, INFO, DAN AGENDA SD AL-IRSYAD 02 CILACAP </h3>
+    1px 1px 0 #000;  ">VISI DAN MISI DARI SD AL-IRSYAD 02 CILACAP </h3>
                     </center>
                 </div>
             </div>
@@ -23,45 +23,35 @@
     </center>
 </div>
 
-<div style="background-color:white; width: auto; padding:100px;" class="row">
-    <div class="col-lg-8 row" style="margin-right:100px;">
-        <!-- PER ARTIKEL -->
-        <?php foreach ($berita as $b) : ?>
-            <div class="col-lg-12 row">
-                <div class="col-lg-4">
-                    <img src="<?= $b["gambar"]; ?>" alt="" style="max-width: 200px; margin-top: 20px;">
-                </div>
-                <div class="col-lg-8 row">
-                    <div class="col-lg-12">
-                        <h3> <a href=""><?= $b["judul"]; ?></a></h3>
-                    </div>
-                    <div class="col-lg-12" style="margin-top:10px;">
-                        <div class="col-lg-3" style="background-color: #FFD500;max-width: 110px;">
-                            <center><?= $b["label"]; ?></center>
-                        </div>
-                        <div class="col-lg-3" style="background-color: #DFE1E1;max-width: 110px;">
-                            <center><?= date("d M Y", $b["tanggal"]); ?></center>
-                        </div>
-                    </div>
-                    <div class="col-lg-12" style="margin-top:20px;">
-                        <p> <?= substr($b["isi"], 0, 200); ?>...</p>
-                    </div>
+<div style="background-color:white; width: auto; padding:100px;" class="container">
 
-                </div>
-                <hr class="col-lg-12">
-            </div>
-        <?php endforeach; ?>
-        <!-- AKHIR PER ARTIKEL -->
-    </div>
-
-    <div style="background-color: #FFD500; width: 275px; height: 200px;" class="col-lg-4">
-        <form action="" style="padding:20px;">
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="Pencarian..." style="width:auto">
-            </div>
-        </form>
-    </div>
-
+    <h3 data-aos="fade-down"> VISI : </h3>
+    <p style="font-size:20px" data-aos="fade-right">
+        TERWUJUDNYA SELURUH POTENSICIVITAS AKADEMIKA MENJADI GENERASI QURANI YANG BERWAWASAN GLOBAL.
+    </p>
+    <h3 data-aos="fade-down">MISI : </h3>
+    <p style="font-size:20px">
+        <ul>
+            <li data-aos="fade-right">Membentuk peserta didik yang berakhlakul karimah dan bertaqwa kepada Allah SWT.</li>
+            <li data-aos="fade-right">Melaksanakan pembelajaran AL-Qur an yang sistematis,terukur dan tersertifikasi.</li>
+            <li data-aos="fade-right">Mengembangkan seluruh potensi peserta didik dibidang akademik dan non akademik.</li>
+            <li data-aos="fade-right">Mengapresiasi seluruh potensi peserta didik dibidang akademik dan non akademik.</li>
+            <li data-aos="fade-right">Menerapkan strategi pembelajaran aktif,inovatif,kreatif,efektif,menyenangkan da islami.</li>
+            <li data-aos="fade-right">Menerapkan strategi pembelajaran aktif,inovatif,kreatif,efektif,menyenangkan da islami</li>
+            <li data-aos="fade-right">Membentuk karakter peserta didik yang kompetitif berbasis qurani.</li>
+            <li data-aos="fade-right">Melibatkan serta peran orang tua dalam mengapresiasi dan mengembangkan bakat dan kompetensi peserata didik.</li>
+        </ul>
+    </p>
+    <h3 data-aos="fade-down">TUJUAN : </h3>
+    <ul>
+        <li data-aos="fade-right">Mewujudakn peserta didik yang berakhlakul karimah dan bertaqwa kepada allah SWT.</li>
+        <li data-aos="fade-right">Terwujudnya peserta didik yg menguasai hafalan Al-Qur an minimal juz 30.</li>
+        <li data-aos="fade-right">Mewujudkan peserta didik yang berprestasi dibidang akademik dan non akademik.</li>
+        <li data-aos="fade-right">Mewujudkan lulusan yang berkarakter,kompetitif,kreatif,inovatif dan sportif.</li>
+        <li data-aos="fade-right">Mewujudkan tegana pendidik dan kependidikan yang profesional.</li>
+        <li data-aos="fade-right">Mewujudkan perangkat kurikulum yang lengkap,mutakhir dan berwawasan kedepan.</li>
+        <li data-aos="fade-right">Terciptanya budaya disiplin,mandiri,jujur dan berani.</li>
+    </ul>
 
 </div>
 
